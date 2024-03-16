1688818b7da27ad81bf56e6184c220dd71b41973
Minh Dinh <macotontai123@gmail.com>
2024-03-16 22:38:50 +0700

    add migration UpdateDatabase2

--- BirthdayParty.DAL/Migrations/20240316153124_UpdateDatabase2.Designer.cs
+++ BirthdayParty.DAL/Migrations/20240316153124_UpdateDatabase2.Designer.cs
@@ -4,6 +4,7 @@ using BirthdayParty.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BirthdayParty.DAL.Migrations
 {
     [DbContext(typeof(BookingPartyContext))]
-    partial class BookingPartyContextModelSnapshot : ModelSnapshot
+    [Migration("20240316153124_UpdateDatabase2")]
+    partial class UpdateDatabase2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
