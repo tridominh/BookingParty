247a6101c1afab04c462fd6afac1485ab58cf082
unknown <tuandtse1772284@fpt.edu.vn>
2024-03-17 20:12:54 +0700

    CRUD user

---
+++ Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty.Services.Interfaces
+{
+	public interface IPaymentService
+	{
+	}
+}
