a90c94f7c782a773057ed5bb4a701c8597f015fd
unknown <tuandtse1772284@fpt.edu.vn>
2024-03-17 23:14:48 +0700

    done CRUD role

---
+++ BirthdayParty.API/Controllers/RoleController.cs
@@ -0,0 +1,67 @@
+﻿using BirthdayParty.Models;
+using BirthdayParty.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirthdayParty.API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class RoleController : ControllerBase
+	{
+		private readonly RoleManager<Role> _roleManager;
+
+		public RoleController(RoleManager<Role> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+
+		[HttpGet("GetAllRoles")]
+		public async Task<ActionResult<IEnumerable<Role>>> GetAllRoles()
+		{
+			var roles = await _roleManager.Roles.ToListAsync();
+			return Ok(roles);
+		}
+
+		[HttpPost("CreateRole")]
+		public async Task<ActionResult<Role>> CreateRole(RoleCreateDTO roleCreateDTO)
+		{
+			var role = new Role(roleCreateDTO.Name);
+			var result = await _roleManager.CreateAsync(role);
+			if (result.Succeeded)
+				return Ok(role);
+			else
+				return BadRequest(result.Errors);
+		}
+
+		[HttpPut("UpdateRole")]
+		public async Task<ActionResult<Role>> UpdateRole(int id, RoleUpdateDTO roleUpdateDTO)
+		{
+			var role = await _roleManager.FindByIdAsync(id.ToString());
+			if (role == null)
+				return NotFound();
+
+			role.Name = roleUpdateDTO.Name;
+			var result = await _roleManager.UpdateAsync(role);
+			if (result.Succeeded)
+				return Ok(role);
+			else
+				return BadRequest(result.Errors);
+		}
+
+		[HttpDelete("DeleteRole")]
+		public async Task<ActionResult<Role>> DeleteRole(int id)
+		{
+			var role = await _roleManager.FindByIdAsync(id.ToString());
+			if (role == null)
+				return NotFound();
+
+			var result = await _roleManager.DeleteAsync(role);
+			if (result.Succeeded)
+				return Ok(role);
+			else
+				return BadRequest(result.Errors);
+		}
+	}
+}
---
+++ BirthdayParty.Models/DTOs/RoleDeleteDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty.Models.DTOs
+{
+	public class RoleDeleteDTO
+	{
+		public int Id { get; set; }
+	}
+}
