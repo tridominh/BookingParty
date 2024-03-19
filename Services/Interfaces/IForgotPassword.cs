8bfa7c3fd4da186fa07779d84c6a9e76c875b4fa
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-03-19 20:55:42 +0700

    validate in room

---
+++ Services/Interfaces/IForgotPassword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty.Services.Interfaces
+{
+    public class IForgotPassword
+    {
+        public interface IForgotPasswordService
+        {
+            Task<bool> ForgotPassword(string email);
+        }
+
+    }
+}
